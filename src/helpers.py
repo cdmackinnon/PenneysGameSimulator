167b6ec82e543eed98d52833d9216c6567ba6baf
cdmackinnon <89928233+cdmackinnon@users.noreply.github.com>
2025-02-17 13:50:40 -0500

    Add Deck Generation
    Add deck generator with a persistent state and deterministic behaviors.

---
+++ src/helpers.py
@@ -0,0 +1,24 @@
+from typing import Callable
+import time
+from datetime import datetime as dt
+
+PATH_DATA = "data"
+
+
+def debugger_factory(show_args=True) -> Callable:
+    def debugger(func: Callable) -> Callable:
+        def wrapper(*args, **kwargs):
+            if show_args:
+                print(f"{func.__name__} was called with:")
+                print("Positional arguments:\n", args)
+                print("Keyword arguments:\n", kwargs)
+
+            t0 = dt.now()
+            results = func(*args, **kwargs)
+            print(f"{func.__name__} ran for {dt.now() - t0}")
+
+            return results
+
+        return wrapper
+
+    return debugger
