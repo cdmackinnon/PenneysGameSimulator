90363a77f91a0e3bea9cdd30b950ecda6a6d10b4
cdmackinnon <89928233+cdmackinnon@users.noreply.github.com>
2025-02-25 00:19:17 -0500

    Add main statemachine
    Added a state machine for improved UX

--- main.py
+++ main.py
@@ -2,11 +2,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+from src.datagen import DeckGenerator
+from src.processing import Evaluator
+from src.visualizing import Plotter
+
+
 
 def main():
-    print(np.random.random())
-    return 42
-
+    while True:
+        print("1. Generate new decks")
+        print("2. Simulate Games")
+        print("3. Plot results")
+        print("Exit\n")
+        choice = input("Enter your choice: ")
+        
+        match choice:
+            case "1":
+                seed = int(input("Enter seed: "))
+                n_decks = int(input("Enter number of decks: "))
+                generator = DeckGenerator(seed)
+                generator.create_decks(n_decks)
+                print(f"Successfully generated {n_decks} decks with seed {seed}")
+            case "2":
+                seed = int(input("Enter seed: "))
+                playerOne = input("Enter player one's choice: ")
+                playerTwo = input("Enter player two's choice: ")
+                evaluator = Evaluator(seed)
+                p1Tricks, p2Tricks, p1Cards, p2Cards = evaluator.evaluate(playerOne, playerTwo)
+                print(f"Player 1 Won {p1Tricks} Game(s) Off Tricks and {p1Cards} Off of Cards")
+                print(f"Player 2 Won {p2Tricks} Game(s) Off Tricks and {p2Cards} Off of Cards\n")
+            case "3":
+                seed = int(input("Enter seed: "))
+                # TODO consider user input for player choices
+                choices = ["000", "001", "010", "011", "100", "101", "110", "111"]
+                plotter = Plotter(choices, seed)
+                plotter.plot_trick_winning_probs()
+                plotter.plot_cards_won_probs()
+            case _:
+                print("Unrecognized input or \"Exit\" entered\nExiting program")
+                break
+                
 
 if __name__ == "__main__":
     main()
