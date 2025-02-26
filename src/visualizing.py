f12fabdf682e28d76a33e879e3f5632bd90466a5
cdmackinnon <89928233+cdmackinnon@users.noreply.github.com>
2025-02-26 13:52:26 -0500

    Update graph information
    Improved graph headings and removed complement mask

--- src/visualizing.py
+++ src/visualizing.py
@@ -3,6 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 from src.processing import Evaluator
+from src.datagen import DeckGenerator
 
 
 class Plotter:
@@ -11,6 +12,7 @@ class Plotter:
         self.df_cards_won_probs = pd.DataFrame()
         self.eval = Evaluator(seed)
         self.playerInputs = playerInputs
+        self.sampleSize = len(DeckGenerator(seed).load_decks())
 
         for playerOne in self.playerInputs:
             for playerTwo in self.playerInputs:
@@ -27,17 +29,19 @@ class Plotter:
     def plot_trick_winning_probs(self):
         self._plot_heatmap(
             self.df_trick_winning_probs,
-            "Penney's Game Player One Wins by Tricks",
+            f"Trick Win Rates Over {self.sampleSize} Games",
         )
 
     def plot_cards_won_probs(self):
         self._plot_heatmap(
-            self.df_cards_won_probs, "Penney's Game Player One Wins by Cards"
+            self.df_cards_won_probs,
+            f"Card Win Rates Over {self.sampleSize} Games",
         )
 
     def _plot_heatmap(self, df, title):
         # Create a mask for the complement values
-        mask = np.triu(np.ones(df.shape, dtype=bool))
+        # mask = np.triu(np.ones(df.shape, dtype=bool))
+        mask = np.eye(df.shape[0], df.shape[1], dtype=bool)
         cmap = sns.diverging_palette(10, 250, as_cmap=True)
 
         plt.figure(figsize=(8, 6))
@@ -52,12 +56,12 @@ class Plotter:
         )
 
         # Adjust label spacing
-        plt.xlabel("Player Two Sequences", fontsize=12, labelpad=12)
-        plt.ylabel("Player One Sequences", fontsize=12, labelpad=12)
+        plt.xlabel("Opponent Sequences", fontsize=12, labelpad=12)
+        plt.ylabel("Player Sequences", fontsize=12, labelpad=12)
         plt.title(title, fontsize=14)
 
         # Adjust tick labels to remove the masked first row and column
-        ax.set_yticks(ax.get_yticks()[1:8], labels=df.index[1:8])
-        ax.set_xticks(ax.get_xticks()[0:7], labels=df.columns[0:7])
+        # ax.set_yticks(ax.get_yticks()[1:8], labels=df.index[1:8])
+        # ax.set_xticks(ax.get_xticks()[0:7], labels=df.columns[0:7])
 
         plt.show()
