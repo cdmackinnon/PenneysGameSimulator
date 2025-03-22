cf17de3a983ec5003a8919d24523ce52c0430b9b
cdmackinnon <89928233+cdmackinnon@users.noreply.github.com>
2025-03-22 17:32:16 -0400

    Simplify Plotting
    Added clearer plot names and simplified the plots into one function to display both at once.

--- src/visualizing.py
+++ src/visualizing.py
@@ -15,16 +15,21 @@ class Plotter:
     def plot_trick_winning_probs(self) -> None:
         self._plot_heatmap(
             self.trick_probs,
-            f"Trick Win Rates Over {self.sampleSize} Games",
+            f"Player Win Rates By Tricks Over {self.sampleSize} Games",
         )
 
     def plot_cards_won_probs(self) -> None:
         self._plot_heatmap(
             self.cards_probs,
-            f"Card Win Rates Over {self.sampleSize} Games",
+            f"Player Win Rates By Cards Over {self.sampleSize} Games",
         )
 
     def _plot_heatmap(self, df: pd.DataFrame, title: str) -> None:
+        # change column names with 1 to R and 0 to B
+        # ex: 111 -> RRR, 000 -> BBB
+        df.index = df.index.str.replace("1", "R").str.replace("0", "B")
+
+
         # Create a mask for shared diagonal values
         mask = np.eye(df.shape[0], df.shape[1], dtype=bool)
         cmap = sns.diverging_palette(10, 250, as_cmap=True)
@@ -45,4 +50,10 @@ class Plotter:
         plt.ylabel("Player Sequences", fontsize=12, labelpad=12)
         plt.title(title, fontsize=14)
 
+        # plt.show()
+    
+    def plot(self) -> None:
+        self.plot_trick_winning_probs()
+        self.plot_cards_won_probs()
         plt.show()
+        
