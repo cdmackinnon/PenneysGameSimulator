4c5c72125cba269f6713cbc4df1f272635d3c276
cdmackinnon <89928233+cdmackinnon@users.noreply.github.com>
2025-03-22 17:31:30 -0400

    Remove Broken Function
    Deleted the simulate games option for the current version.

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
 from src.datagen import DeckGenerator
-from src.processing import Evaluator
 from src.visualizing import Plotter
 
 
@@ -11,8 +6,8 @@ from src.visualizing import Plotter
 def main():
     while True:
         print("1. Generate new decks")
-        print("2. Simulate Games")
-        print("3. Plot results")
+        # print("X. Simulate Games")
+        print("2. Plot results")
         print("Exit\n")
         choice = input("Enter your choice: ")
         
@@ -23,19 +18,21 @@ def main():
                 generator = DeckGenerator(seed)
                 generator.create_decks(n_decks)
                 print(f"Successfully generated {n_decks} decks with seed {seed}\n")
+            # TODO: Implement this feature in the future
+            # case "X":
+            #     seed = int(input("Enter seed: "))
+            #     playerOne = input("Enter player one's choice: ")
+            #     playerTwo = input("Enter player two's choice: ")
+            #     evaluator = Evaluator(seed)
+            #     p1Tricks, p2Tricks, p1Cards, p2Cards = evaluator.evaluate_decks(playerOne, playerTwo)
+            #     print(f"Player 1 Won {p1Tricks} Game(s) Off Tricks and {p1Cards} Off of Cards")
+            #     print(f"Player 2 Won {p2Tricks} Game(s) Off Tricks and {p2Cards} Off of Cards\n")
             case "2":
                 seed = int(input("Enter seed: "))
-                playerOne = input("Enter player one's choice: ")
-                playerTwo = input("Enter player two's choice: ")
-                evaluator = Evaluator(seed)
-                p1Tricks, p2Tricks, p1Cards, p2Cards = evaluator.evaluate(playerOne, playerTwo)
-                print(f"Player 1 Won {p1Tricks} Game(s) Off Tricks and {p1Cards} Off of Cards")
-                print(f"Player 2 Won {p2Tricks} Game(s) Off Tricks and {p2Cards} Off of Cards\n")
-            case "3":
-                seed = int(input("Enter seed: "))
                 plotter = Plotter(seed)
-                plotter.plot_trick_winning_probs()
-                plotter.plot_cards_won_probs()
+                # plotter.plot_trick_winning_probs()
+                # plotter.plot_cards_won_probs()
+                plotter.plot()
             case _:
                 print("Unrecognized input or \"Exit\" entered\nExiting program")
                 break
